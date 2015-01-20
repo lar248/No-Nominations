37f0b954fb507b4b0a1bcffda388b843be232875
Laurence Rosenzweig <laurencealanrosenzweig@gmail.com>
2015-01-19 21:59:51 -0500

    clicking radio buttons works

--- app.js
+++ app.js
@@ -8,11 +8,26 @@
 			}).
 			error(function(data) {
 				console.log("error yooo")
-			})
-		
-	});
-	app.controller('FormController', function($scope) {
-		$scope.formData = {};
-		
+			});
+		//$scope.formData = [];
+		var formData = [];
+		console.log("yo")
+		$scope.addNomToForm = function(short, nomination) {
+			var category = {};
+			category.nomination = nomination;
+			category.short = short;
+			var index = -1;
+			for (var i=0; i<formData.length; i++) {
+				if (angular.equals(formData[i].short, short)) {
+					index = i;
+				}
+			}
+			if (index!=-1) {
+				formData.splice(index, 1, category)
+			} else {
+				formData.push(category);
+			}
+			console.log(formData)
+		};
 	});
 })();
