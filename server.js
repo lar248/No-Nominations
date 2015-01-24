f7d1b1047739bbd084fd507a752f8d1a18f71445
Laurence Rosenzweig <laurencealanrosenzweig@gmail.com>
2015-01-24 11:31:52 -0500

    modified server.js

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
 
+// ROUTES
+require('./app/routes.js')(app);
+
 app.listen(port);
 console.log("App listening on port " + port);
 
