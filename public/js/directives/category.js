be5303f13496f8c0099315cd6d7d97046d1bbc53
Laurence Rosenzweig <laurencealanrosenzweig@gmail.com>
2015-01-28 21:47:24 -0500

    edit modal works

--- public/js/directives/category.js
+++ public/js/directives/category.js
@@ -4,18 +4,13 @@
 		return {
 			restrict: 'A',
 			link: function(scope, elem, attrs) {
-				var category = elem;
-				 
 				$(elem).click(function() {
+					var categoryNoms = elem.next().children();
 					var dialog = $( "#dialog-form" ).dialog({
 						autoOpen: false,
 						width: 600,
 						modal: true,
 						open: function(event) {
-							console.log("event", event)
-							console.log('THIS elem', category)
-							console.log('next elem', category.next())
-							var categoryNoms = elem.next().children();
 							var heading = document.createElement("tr");
 							var movie = "<th>Movie</th>";
 							var detail = "<th>Details</th>";
@@ -38,11 +33,22 @@
 							});	
 						}, 
 						buttons: {
-							
 							Cancel: function() {
 								$('#modal-background').css('display', 'none');
 							  	dialog.dialog("close");
 							  	$('#modal').empty();
+							},
+							Submit: function() {
+								$('#modal-background').css('display', 'none');
+								$(categoryNoms).each(function(i) {
+									var modalRow = $('#modal tbody tr')[i+1];
+									$($(this).children("input")).val($(modalRow).children().children("input[name='movie']").val());
+									$($(this).children("a")).html($(modalRow).children().children("input[name='movie']").val());
+									$($(this).children("span")).html($(modalRow).children().children("input[name='detail']").val()); 
+									$($(this).children("a")).attr('href', $(modalRow).children().children("input[name='imdb']").val()); 
+								});
+								dialog.dialog("close");
+								$('#modal').empty();
 							}
 						}
 				    });
@@ -53,5 +59,4 @@
 			}
 		}
 	});
-
 })();
