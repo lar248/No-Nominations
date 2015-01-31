94cfd76151f90ddb189fae900cf38e9040cae0d3
Laurence Rosenzweig <laurencealanrosenzweig@gmail.com>
2015-01-31 17:18:58 -0500

    signup template works

--- server.js
+++ server.js
@@ -44,6 +44,7 @@ app.get('/users', function (req, res) {
 
 // CONFIGURATION
 // required for express
+
 app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
 app.use(morgan('dev')); // log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
@@ -51,7 +52,8 @@ app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
 
-app.set('view engine', 'ejs'); // set up ejs for templating
+app.engine('ejs', require('ejs').renderFile);
+app.set('view engine', 'ejs'); //set up ejs for templating
 
 // required for passport
 app.use(cookieParser());
