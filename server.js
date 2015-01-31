301a650d4c3c90acbb18456841afb1b39eb4fe0f
Laurence Rosenzweig <laurencealanrosenzweig@gmail.com>
2015-01-31 15:44:58 -0500

    setting up login

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ var database = require('./config/database'); //Need to update the database later
 var morgan   = require('morgan'); //for logging requests to console
 var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
-
-
+var passport = require('passport');
+var flash    = require('connect-flash');
+var session      = require('express-session');
+var cookieParser = require('cookie-parser');
 
 
 // DATABASE CONFIGURATION
@@ -38,8 +40,10 @@ app.get('/users', function (req, res) {
 });
 */
 
+// require('./config/passport')(passport); // pass passport for configuration
 
 // CONFIGURATION
+// required for express
 app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
 app.use(morgan('dev')); // log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
@@ -47,8 +51,20 @@ app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
 
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(cookieParser());
+app.use(session({ 
+    secret: 'oneflewoverthecuckoosnest',
+    resave: false 
+})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
 // ROUTES
-require('./app/routes.js')(app);
+require('./app/routes.js')(app, passport);
 
 app.get('/hi', function(req, res) {
 	//res.send('hello world yo');
