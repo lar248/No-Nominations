f8690e59343da78df1d3abae98ab75fe6b531cd7
Laurence Rosenzweig <laurencealanrosenzweig@gmail.com>
2015-02-02 00:29:01 -0500

    Facebook oauth works

--- app/routes.js
+++ app/routes.js
@@ -24,6 +24,25 @@ function isLoggedIn(req, res, next) {
 module.exports = function(app, passport) {
 	// API ----
 	// =====================================
+    // FACEBOOK ROUTES =====================
+    // =====================================
+    // route for facebook authentication and login
+    app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+
+    // handle the callback after facebook has authenticated the user
+    app.get('/auth/facebook/callback',
+        passport.authenticate('facebook', {
+            successRedirect : '/profile',
+            failureRedirect : '/'
+        }));
+
+    // route for logging out
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
+	// =====================================
     // LOGIN ===============================
     // =====================================
     // show the login form
---
+++ config/auth.js
@@ -0,0 +1,9 @@
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '356126467927376', // your App ID
+        'clientSecret'  : '5876ad6070ad1fc6c07fc01488a8c76d', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    }
+};
